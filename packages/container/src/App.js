88ebef7ba2ecc7d7361a838dabd8b028a2a16611
Nikola Trajkovic <nikola.trajkovic@bestow.com>
2022-04-25 11:17:56 +0200

    change inside of container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>test marketing app</h1>
+      <h1>Test marketing app</h1>
       <MarketingApp />
     </div>
   );
